ac8a7c85ded6af096132620bd3a15a65f2beb056
Fasasi Toheeb <tohebayomide11@gmail.com>
2021-08-22 08:12:33 +0100

    Fixed bug

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 
 const oAuth2Client = new google.auth.OAuth2(
       process.env.CLIENT_ID,
-      process.env.CLEINT_SECRET,
+      process.env.CLIENT_SECRET,
       process.env.REDIRECT_URI
     );
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
@@ -18,7 +18,7 @@ const sendEmail = async (receiverEmail, subject, message, name) => {
         type: 'OAuth2',
         user: process.env.SENDER_EMAIL,
         clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLEINT_SECRET,
+        clientSecret: process.env.CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
         accessToken: accessToken,
       },
